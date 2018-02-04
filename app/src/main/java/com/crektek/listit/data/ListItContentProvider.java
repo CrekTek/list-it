a3d01a1fcb43cded6bdcf8b415e761a66171ca44
George Salter <george.salter93@gmail.com>
2018-02-04 15:45:40 +0000

    Complete insert method in content provider

--- app/src/main/java/com/crektek/listit/data/ListItContentProvider.java
+++ app/src/main/java/com/crektek/listit/data/ListItContentProvider.java
@@ -1,18 +1,24 @@
 package com.crektek.listit.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by George on 03/02/2018.
  */
 
 public class ListItContentProvider extends ContentProvider {
+
+    private static final String TAG = ListItContentProvider.class.getSimpleName();
 
     public static final int ITEMS = 100;
     public static final int ITEM_WITH_ID = 101;
@@ -21,7 +27,9 @@ public class ListItContentProvider extends ContentProvider {
     public static final int LIST_WITH_ID = 201;
     public static final int ITEMS_IN_LIST = 202;
 
-    private static final UriMatcher uriMatcher = buildUriMatcher();
+    private static final UriMatcher mUriMatcher = buildUriMatcher();
+
+    private ListItDbHelper mListItDbHelper;
 
     private static UriMatcher buildUriMatcher() {
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
@@ -39,7 +47,9 @@ public class ListItContentProvider extends ContentProvider {
 
     @Override
     public boolean onCreate() {
-        return false;
+        Context context = getContext();
+        mListItDbHelper = new ListItDbHelper(context);
+        return true;
     }
 
     @Nullable
@@ -57,7 +67,36 @@ public class ListItContentProvider extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
-        return null;
+
+        Log.d(TAG, "Dealing with insert uri: " + uri);
+
+        SQLiteDatabase db = mListItDbHelper.getWritableDatabase();
+
+        int match = mUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match) {
+            case ITEMS:
+                long itemId = db.insert(ListItContract.ItemEntry.TABLE_NAME, null, contentValues);
+                if ( itemId > 0 ) {
+                    returnUri = ContentUris.withAppendedId(ListItContract.ItemEntry.CONTENT_URI, itemId);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            case LISTS:
+                long listId = db.insert(ListItContract.ListEntry.TABLE_NAME, null, contentValues);
+                if ( listId > 0 ) {
+                    returnUri = ContentUris.withAppendedId(ListItContract.ListEntry.CONTENT_URI, listId);
+                } else {
+                    throw new android.database.SQLException("Failed to insert row into " + uri);
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        return returnUri;
     }
 
     @Override
