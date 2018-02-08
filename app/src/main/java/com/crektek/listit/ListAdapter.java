f672bb3c23fa0806a08ad1c29ca6e5ecb5c3ce6e
George Salter <george.salter93@gmail.com>
2018-02-08 10:48:25 +0000

    List display now correctly displaying lists when added

--- app/src/main/java/com/crektek/listit/ListAdapter.java
+++ app/src/main/java/com/crektek/listit/ListAdapter.java
@@ -1,28 +1,31 @@
 package com.crektek.listit;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.crektek.listit.data.ListItContract;
+
 /**
- * Created by George on 23/01/2018.
+ * Created on 23/01/2018.
  */
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListItemViewHolder> {
 
-    private String[] mDataSet;
+    private Context mContext;
+    private Cursor mCursor;
 
-    public ListAdapter(String[] dataSet) {
-        mDataSet = dataSet;
+    public ListAdapter(Context context) {
+        mContext = context;
     }
 
     @Override
     public ListItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
         int layoutIdForListItem = R.layout.list_item;
-        LayoutInflater inflater = LayoutInflater.from(context);
+        LayoutInflater inflater = LayoutInflater.from(mContext);
         boolean shouldAttachToParentImmediately = false;
 
         View view = inflater.inflate(layoutIdForListItem, parent, shouldAttachToParentImmediately);
@@ -33,12 +36,49 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ListItemViewHo
 
     @Override
     public void onBindViewHolder(ListItemViewHolder holder, int position) {
-        holder.mItemText.setText(mDataSet[position]);
+
+        int idIndex = mCursor.getColumnIndex(ListItContract.ListEntry._ID);
+        int titleIndex = mCursor.getColumnIndex(ListItContract.ListEntry.COLUMN_NAME_TITLE);
+        int typeIndex = mCursor.getColumnIndex(ListItContract.ListEntry.COLUMN_NAME_TYPE);
+
+        mCursor.moveToPosition(position);
+
+        final int id = mCursor.getInt(idIndex);
+        final String title = mCursor.getString(titleIndex);
+
+        holder.itemView.setTag(id);
+        holder.mItemText.setText(title);
     }
 
     @Override
     public int getItemCount() {
-        return mDataSet.length;
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
+    }
+
+    /**
+     * When the data changes a re-query occurs. This will swap the old data
+     * for new data.
+     *
+     * @param cursor the new data passed in
+     * @return the old data
+     */
+    public Cursor swapCursor(Cursor cursor) {
+
+        if(mCursor == cursor) {
+            return null;
+        }
+
+        Cursor oldCursor = mCursor;
+        mCursor = cursor;
+
+        if(mCursor != null) {
+            notifyDataSetChanged();
+        }
+
+        return oldCursor;
     }
 
     public static class ListItemViewHolder extends RecyclerView.ViewHolder {
--- app/src/main/java/com/crektek/listit/MainActivity.java
+++ app/src/main/java/com/crektek/listit/MainActivity.java
@@ -1,17 +1,31 @@
 package com.crektek.listit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.crektek.listit.data.ListItContract;
+
+import java.lang.ref.WeakReference;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int LIST_LOADER_ID = 0;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
-    private RecyclerView.Adapter mListAdapter;
+    private ListAdapter mListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +41,81 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        String[] data = {"item 1", "item 2", "item 3"};
-
-        mListAdapter = new ListAdapter(data);
+        mListAdapter = new ListAdapter(this);
         mRecyclerView.setAdapter(mListAdapter);
+
+        getSupportLoaderManager().initLoader(LIST_LOADER_ID, null, this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // re-queries for all tasks
+        getSupportLoaderManager().restartLoader(LIST_LOADER_ID, null, this);
     }
 
     public void onClickLaunchAddListActivity(View view) {
         Intent addListIntent = new Intent(MainActivity.this, AddListActivity.class);
         startActivity(addListIntent);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new LoadDataTask(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mListAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mListAdapter.swapCursor(null);
+    }
+
+    private static class LoadDataTask extends AsyncTaskLoader<Cursor> {
+
+        private WeakReference<Context> mContext;
+
+        private Cursor mListData = null;
+
+        public LoadDataTask(Context context) {
+            super(context);
+            mContext = new WeakReference<>(context);
+        }
+
+        @Override
+        protected void onStartLoading() {
+            if (mListData != null) {
+                // Delivers any previously loaded data immediately
+                deliverResult(mListData);
+            } else {
+                forceLoad();
+            }
+        }
+
+        @Override
+        public Cursor loadInBackground() {
+            try {
+                return mContext.get().getContentResolver().query(ListItContract.ListEntry.CONTENT_URI,
+                        null,
+                        null,
+                        null,
+                        ListItContract.ListEntry.COLUMN_NAME_PRIORITY);
+
+            } catch (Exception e) {
+                Log.e(TAG, "Failed to asynchronously load data.");
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        public void deliverResult(Cursor data) {
+            mListData = data;
+            super.deliverResult(data);
+        }
     }
 }
